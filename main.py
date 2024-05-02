e815d3f472af7466c7fd323567ba37e32c77c534
morpha <v21.leonardo@gmail.com>
2024-05-02 15:40:28 -0300

    main.py shows a performance comparison between running some calculation sequentialy, via multithreading, and via multiprocessing

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 from time import perf_counter
 from threading import Thread
+from multiprocessing import Process
 
-from utils.num_functions import is_perfect
+from utils.num_functions import perfect_finder
 from utils.list_utils import splitter
 
-numbers = [i for i in range(2, 30000)]
+numbers = [i for i in range(2, 200_000)]
 
 # Measuring execution time of cpu intensive task made sequentialy:
+print("#----- Sequential -----#")
 t = perf_counter()
-for i in numbers:
-	if is_perfect(i):
-		print(i)
+perfect_finder(numbers)
 t = perf_counter()-t
-print()
-print()
 print(f"time elapsed: {t:.4f}s")
+print()
+print()
 
-
+# splitting workload
 numbers = splitter(numbers, 7)
 
+# preparing threads
+threads = [Thread(target=perfect_finder, args=[i]) for i in numbers]
 
+print("#--- Multithreading ---#")
+t = perf_counter()
+for thread in threads:
+	thread.start()
+for thread in threads:
+	thread.join()
+t = perf_counter() - t
+print(f"time elapsed: {t:.4f}s")
+print()
+print()
 
-# Measuring execution time of cpu intensive task made in multithreading:
-#t = perf_counter()
+# preparing processes
+processes = [Process(target=perfect_finder, args=[i]) for i in numbers]
 
-
-
-#t = perf_counter()-t
-#print()
-#print()
-#print(f"time elapsed: {t:.4f}s")
-
-
-
+print("#-- Multiprocessing ---#")
+t = perf_counter()
+for process in processes:
+	process.start()
+for process in processes:
+	process.join()
+t = perf_counter() - t
+print(f"time elapsed: {t:.4f}s")
+print()
--- utils/num_functions.py
+++ utils/num_functions.py
@@ -6,6 +6,10 @@ def divisors_sum(num):
 def is_perfect(num):
 	return num == divisors_sum(num)
 
+def perfect_finder(numbers):
+	for i in numbers:
+		if is_perfect(i):
+			print(i)
 
 if __name__ == "__main__":
 	for i in range(2, 34000000):
